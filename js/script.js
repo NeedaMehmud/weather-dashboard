76aaf36b01d885a73022c85e532f6544708076e7
Needa Mehmud <needa.mehmud@gmail.com>
2021-06-28 22:11:15 -0400

    got 5 day forecast working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var apiKey = 'bd7825a30dfe9f79399ab02e7685f767';
+var apiKey = '65d1ed00266eb90ad5d1a2f983cb093f';
 var searchCityBtn = $('#searchCityButton');
 var searchCityInput = $('#inputCity');
-// var currentDay = moment().format("dddd, MMMM Do YYYY");
+var currentDay = moment().format("dddd, MMMM Do YYYY");
 
 
 searchCityBtn.on("click", function (event) {
@@ -20,8 +20,7 @@ searchCityBtn.on("click", function (event) {
 console.log(searchCityBtn)
 
 function getWeather(cityName) {
-    // api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}
-    let queryCityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey + '&units=imperial';
+    let queryCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
     console.log(queryCityUrl);
     fetch(queryCityUrl)
         .then(response => response.json())
@@ -31,11 +30,33 @@ function getWeather(cityName) {
             console.log(data.main.temp);
 
             $('#temp').text("Temperature: " + data.main.temp + '°F');
-            $('#wind').text("Wind Speed: " + data.wind.speed);
+            $('#wind').text("Wind Speed: " + data.wind.speed + 'MPH');
             $('#humidity').text("Humidity: " + data.main.humidity + '%');
             $('#location').text(data.name);
             $('#cloudy').text(data.weather.main);
             $('#date').append(currentDay);
+
+            var latt = data.coord.lat;
+            var long = data.coord.lon;
+            let fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latt}&lon=${long}&units=imperial&exclude=minutely,hourly,alerts&appid=${apiKey}`;
+
+            console.log(fiveDayUrl);
+            fetch(fiveDayUrl)
+                .then(response => response.json())
+                .then(data => {
+                    for (var i = 1; i < data.daily.length; i++) {
+                        var momentDate = moment.unix(data.daily[i].dt);
+                        var day = momentDate.format('ddd');
+                        $(`#day${i}`).text(day);
+                        console.log(day);
+                        // add data to five-day html
+                        $(`#day${i}temp`).text("Temp: " + data.daily[i].temp.day + '°F');
+                        $(`#day${i}humidity`).text("Humidity: " + data.daily[i].humidity + '%');
+                        $(`#day${i}windspeed`).text("Wind: " + data.daily[i].wind_speed + 'MPH');
+
+                    }
+                })
+
         });
 
 }
